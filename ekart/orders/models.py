5d8a50798291ace668952e6a50dbe927b8376769
Joshwa Sanz <joshwasanz@gmail.com>
2025-02-27 15:11:34 -0500

    orders model created

--- ekart/orders/models.py
+++ ekart/orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from customers.models import Customer
+from products.models import Product
 
 # Create your models here.
+
+class Order(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES = (
+        (LIVE,'LIVE'),
+        (DELETE,'DELETE')
+    )
+
+    owner = models.ForeignKey(Customer,on_delete=models.SET_NULL,related_name="orders")
+    delete_status = models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OrderedItem(models.Model):
+    products = models.ForeignKey(Product)
+    quantity = models.IntegerField(default=1)
